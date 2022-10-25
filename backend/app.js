19bc8f105cd7057fadfa6f773110dc73a888e4b6
nata-naumova <naumonata@yandex.ru>
2022-10-25 15:08:01 +0300

    new update

--- backend/app.js
+++ backend/app.js
@@ -1,64 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const mainErrors = require('./middlewares/main-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
-const { PORT = 3000 } = process.env;
-const app = express();
 const routes = require('./routes/routes');
 
-// подключаемся к серверу Mongo
-async function main() {
-  try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
-  }
-  app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`App listening on port ${PORT}`);
-  });
-}
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(helmet());
 
 // МИДЛВАРЫ
 app.use(bodyParser.json());
-
-// Подключаем роуты
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
 
-// Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'https://mesto.nata.nomoredomains.icu/',
-  'http://mesto.nata.nomoredomains.icu/',
-  'localhost:3000'
-];
+app.use(cors());
 
-app.use(function(req, res, next) {
-  const { method } = req;
-  const { origin } = req.headers;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  return next();
-});
-
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+app.use(express.json()); // Подключаем роуты
 
 app.use(routes);
 
@@ -66,6 +29,17 @@ app.use(routes);
 app.use(errorLogger);
 app.use(errors());
 app.use(mainErrors);
+
+// подключаемся к серверу Mongo
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+  // eslint-disable-next-line no-console
+  console.log('Connected to db');
+
+  await app.listen(PORT);
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+}
 
 // Запуск сервера
 main();
