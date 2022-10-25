137f3c2681e4c8b7006fcefaebff5fb785e62b87
nata-naumova <naumonata@yandex.ru>
2022-10-25 18:39:09 +0300

    update code

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-//const cors = require('cors');
 const helmet = require('helmet');
 
 const { errors } = require('celebrate');
--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://auth.nomoreparties.co";
+export const BASE_URL = "https://api.mesto.nata.nomoredomains.icu";
 
 export const register = (password, email) => {
     return fetch(`${BASE_URL}/signup`, {
