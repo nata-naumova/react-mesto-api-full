0c506c8901ed3673f55b0d27d30abed839193386
nata-naumova <naumonata@yandex.ru>
2022-10-25 20:39:50 +0300

    update url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -92,9 +92,8 @@ class Api {
     }
 }
 const api = new Api({
-    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-45',
+    baseUrl: 'https://api.mesto.nata.nomoredomains.icu/',
     headers: {
-        authorization: '3e8431a3-54b0-494c-b0b7-18b456b2213e',
         'Content-Type': 'application/json'
     }
 });
