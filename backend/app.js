6dab1d69790b08b1261946c8c4959cecf392800c
nata-naumova <naumonata@yandex.ru>
2022-10-26 21:09:16 +0300

    fix: mongoose

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,8 @@ app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(helmet());
 app.use(requestLogger);
 
