24ad1243271f62380c8e57ca0a78a3aaf9eaa4ad
nata-naumova <naumonata@yandex.ru>
2022-10-26 11:59:04 +0300

    update code backend cors

--- backend/app.js
+++ backend/app.js
@@ -20,37 +20,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(requestLogger);
 
-/*
-const allowedCors = [
-  // eslint-disable-next-line quotes
-  "https://mesto.nata.nomoredomains.icu",
-  // eslint-disable-next-line quotes
-  "http://mesto.nata.nomoredomains.icu",
-  // eslint-disable-next-line quotes
-  "http://localhost:3000",
-];
+const corsOptions = {
+  origin: ['https://mesto.nata.nomoredomains.icu'],
+};
 
-app.use((req, res, next) => {
-  const { method } = req;
-  const { origin } = req.headers;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  return next();
-});
-*/
-
-app.use(cors({
-  Origin: 'https://mesto.nata.nomoredomains.icu',
-}));
+app.use(cors(corsOptions));
 
 app.get('/crash-test', () => {
   setTimeout(() => {
