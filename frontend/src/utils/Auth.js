d4b6df1e3c5cd9165d46b7499bae0c3bfbf789b9
nata-naumova <naumonata@yandex.ru>
2022-10-27 13:34:03 +0300

    update frontend app

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,65 +1,60 @@
-import { optionsApi } from "./optionsApi";
-
-export const register = ({ password, email }) => {
-    return fetch(optionsApi.baseUrl + 'signup', {
-        method: "POST",
-        headers: {
-            "Accept": 'application/json',
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ password, email }),
-    })
-    .then(res => {
-        if (res.ok) {
-            return res.json();
-        }
-        if (res.status === 400) {
-            return Promise.reject(`Ошибка: некорректно заполнено одно из полей`)
-        }
-        return Promise.reject(`Ошибка: ${res.status}`)
-    })
-};
-
-export const authorize = ({ password, email }) => {
-    return fetch(optionsApi.baseUrl + 'signin', {
-        method: "POST",
-        headers: {
-            "Accept": 'application/json',
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ password, email }),
-    })
-    .then(res => {
-        if (res.ok) {
-            return res.json();
-        }
-        if (res.status === 400) {
-            return Promise.reject(`Ошибка: не передано одно из полей`)
-        } else if (res.status === 401) {
-            return Promise.reject(`Ошибка: пользователь с email не найден`)
-        }
-        return Promise.reject(`Ошибка: ${res.status}`)
-    })
-};
-
-export const checkToken = (token) => {
-    return fetch(optionsApi.baseUrl + 'users/me', {
+class Auth {
+    constructor({ baseUrl, headers }) {
+      this._baseUrl = baseUrl;
+      this._headers = headers;
+    }
+  
+    _checkResponse(res) {
+      if (res.ok) {
+        return res.json();
+      }
+  
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+  
+    register(email, password) {
+      return fetch(`${this._baseUrl}/signup`, {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      })
+      .then(this._checkResponse);
+    }
+  
+    authorize(email, password) {
+      return fetch(`${this._baseUrl}/signin`, {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      })
+      .then(this._checkResponse);
+    }
+  
+    checkToken(token)  {
+      return fetch(`${this._baseUrl}/users/me`, {
         method: 'GET',
         headers: {
-            "Accept": 'application/json',
-            "Content-Type": "application/json",
-            "Authorization": 'Bearer ' + token
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
         }
-    })
-    .then(res => {
-        if (res.ok) {
-            return res.json();
-        }
-        if (res.status === 400) {
-            return Promise.reject(`Ошибка: токен не передан или передан не в том формате`)
-        } else if (res.status === 401) {
-            return Promise.reject(`Ошибка: переданный токен некорректен`)
-        }
-        return Promise.reject(`Ошибка: ${res.status}`)
-    })
-} 
+      })
+      .then(this._checkResponse);
+    }
+  }
+  
+  const auth = new Auth({
+    baseUrl: 'https://mesto.nata.nomoredomains.icu',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }
+  });
+  
+  export default auth;
