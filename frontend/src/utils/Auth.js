effb1ceec157a35a2345f1d85117572e148090aa
nata-naumova <naumonata@yandex.ru>
2022-10-27 15:18:47 +0300

    update frontend auth

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -12,7 +12,7 @@ class Auth {
       return Promise.reject(`Ошибка: ${res.status}`);
     }
   
-    register(email, password) {
+    register({ email, password }) {
       return fetch(`${this._baseUrl}/signup`, {
         method: 'POST',
         headers: this._headers,
@@ -24,7 +24,7 @@ class Auth {
       .then(this._checkResponse);
     }
   
-    authorize(email, password) {
+    authorize({ email, password }) {
       return fetch(`${this._baseUrl}/signin`, {
         method: 'POST',
         headers: this._headers,
