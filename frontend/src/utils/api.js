c6ea45ca496043dd479923429c1b79674a4cc92b
nata-naumova <naumonata@yandex.ru>
2022-10-30 12:49:48 +0300

    обновила ссылки на фронте

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -104,7 +104,7 @@ class Api {
     }
 }
 const api = new Api({
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://api.mesto.nata.nomoredomains.icu',
     headers: {
         //authorization: '3e8431a3-54b0-494c-b0b7-18b456b2213e',
         'Content-Type': 'application/json'
